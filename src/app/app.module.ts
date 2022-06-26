89d476a31984636af014f3140e2913121e9e818b
Abhishek-S-777 <s.abhishek.official@gmail.com>
2022-06-26 18:18:37 +0530

    Created Components, designed top songs and artists

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './AlphaComponents/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './AlphaComponents/login/login.component';
+import { ArtistCardComponent } from './AlphaComponents/artist-card/artist-card.component';
+import { SongCardComponent } from './AlphaComponents/song-card/song-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    ArtistCardComponent,
+    SongCardComponent
   ],
   imports: [
     BrowserModule,
